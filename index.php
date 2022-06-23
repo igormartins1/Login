4eed963cf6b1ddaa1cd916c4bc3c49d93e9793aa
igormartins1 <igor.1294690@discente.uemg.br>
2022-06-22 21:16:27 -0300

    Estr html

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Fontawsome-->
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+    <title>Login e Cadastro</title>
+</head>
+<body>
+    <div class="container">
+        <div class="form-control">
+            <div class="signin-signup">
+                <form action="" class="sign-in-form">
+                    <h2 class="title">Sign-in</h2>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Username">
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="password" placeholder="Password">
+                    </div>
+                </form>
+            </div>
+
+        </div>
+
+
+        <div class="panels-container"></div>
+    </div>
+    
+</body>
+</html>
